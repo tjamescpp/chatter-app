dd0f7fa7d198867b4b6a38642f0a061efe95234a
ttjamescpp <jamestommy.611@gmail.com>
2025-06-11 14:50:13 -0700

    fixed port binding for Render

--- manage.py
+++ manage.py
@@ -9,6 +9,13 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twtapp.settings")
     try:
         from django.core.management import execute_from_command_line
+
+        # Get port from environment or default to 8000
+        port = os.environ.get("PORT", "8000")
+        args = sys.argv
+
+        if len(args) == 1 or (args[1] == "runserver" and len(args) == 2):
+            args += [f"0.0.0.0:{port}"]
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
